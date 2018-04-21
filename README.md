accdb5b0396c16b36c1f88227beb282bc3768598
Dime Mikhalchenko <mixal4enko@gmail.com>
2018-04-22 01:47:38 +0300

    working simplest calc

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # bash_fun
 Just some bash programs to learn bash and use it as a real programming language
 
-* Snake game
-* Polish notaion calc (like lisp)
+* Snake game, just start and play
+* Polish notaion calc (like lisp): `echo "(* 3 (+ 1 2))" | bash lispcalc.sh`
--- lispcalc.sh
+++ lispcalc.sh
@@ -6,6 +6,8 @@
 # (* 7 3) -> 21
 # (+ (* 2 3) 5) -> 30
 
+
+# some functional programming stuff
 map () {
     local func=$1
     shift
@@ -32,8 +34,12 @@ _double () {
     echo $(( $1 * 2 ))
 }
 
-_sum2  () {
+_sum2 () {
     echo $(( $1 + $2 ))
+}
+
+_mul2 () {
+    echo $(( $1 * $2 ))
 }
 
 _test () {
@@ -48,7 +54,53 @@ _test () {
 }
     
 
-_test
+#_test
 
-     
 
+
+#################################################
+
+__plus__ () {
+    reduce _sum2 0 $*
+}
+
+__mult__ () {
+    reduce _mul2 1 $*
+}
+
+tokenize () {
+    sed 's/(\|)/ & /g; s/\s\s*/\n/g' | grep -v "^$"
+}
+
+evaluate () {
+    local func
+    local token
+    local args=( )
+
+    read func
+
+    case $func in
+        '*')
+            func='__mult__'
+            ;;
+        '+')
+            func='__plus__'
+            ;;
+    esac
+
+    while read token; do
+        case $token in
+            ')')
+                echo $($func ${args[*]})
+                return 0
+                ;;
+            '(')
+                args=( ${args[*]} `evaluate` )
+                ;;
+            *)
+                args=( ${args[*]} $token )
+        esac
+    done
+}
+
+tokenize | tail -n+2 | evaluate
