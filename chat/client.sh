c0fbb5bb1c7776dae844462472e8b44de4ce116c
Dime Mikhalchenko <mixal4enko@gmail.com>
2018-06-04 17:37:49 +0300

    fix waiting for server reply

--- chat/client.sh
+++ chat/client.sh
@@ -3,22 +3,25 @@
 PORT=3001
 HOST=localhost
 
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <USERNAME>"
+    exit 1
+fi
 NAME=$1
 
-new_port=$(echo "HELLO" | nc $HOST $PORT)
+new_port=$(echo "HELLO" | nc $HOST $PORT -q 1)
 echo "new port: $new_port" >&2
 
 fifo_dir=$(mktemp -d)
-echo "fifo dir: $fifo_dir" >&2
+#echo "fifo dir: $fifo_dir" >&2
 
 incoming="${fifo_dir}/incoming"
 outgoing="${fifo_dir}/outgoing"
-echo "incoming: $incoming" >&2
-echo "fifo dir: $outgoing" >&2
+#echo "incoming: $incoming" >&2
+#echo "fifo dir: $outgoing" >&2
 
 mkfifo $incoming
 mkfifo $outgoing
-
 
 exec 3<> $incoming
 exec 4<> $outgoing
