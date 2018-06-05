1da8473ea5ffb5fcb740894b1746012885fc024d
Dime Mikhalchenko <mixal4enko@gmail.com>
2018-06-05 22:38:24 +0300

    stream multiplexing

--- little_snippets/snippets.sh
+++ little_snippets/snippets.sh
@@ -1,6 +1,11 @@
-#####
+### quotes magic
 a='*'
 
 echo $a
 echo '$a'
 echo "$a"
+
+### stream multiplexing 
+cat | tee >(grep foo | wc -l > foo.count)\
+          >(grep baz | wc -l > baz.count)\
+          |(grep egg | wc -l > egg.count)
